10db2200486673de9e4b41f4f2cff5508d093d61
2016HT13546 <2016HT13546@wilp.bits-pilani.ac.in>
2018-07-03 00:40:57 +0530

    Binomial Coefficient

---
+++ src/com/sample/test/BinomialCoefficient.java
@@ -0,0 +1,25 @@
+package com.sample.test;
+
+public class BinomialCoefficient {
+
+	public static int findBinomialCoefficient(int n, int k) {
+		int dp[][] = new int[n+1][k+1];
+		
+		for ( int i=0; i<=n; i++ ) {
+			for ( int j=0; j <= Math.min (i, k); j++ ) {
+				if ( j==0 || j==i) {
+					dp[i][j] = 1;
+				}else {
+					dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+				}
+			}
+		}
+		
+		return dp[n][k];
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(findBinomialCoefficient(4, 2));
+	}
+	
+}
